31f823304d4e636884fefe8fa717ae051212ca4f
flbrasington <brasingf@oregonstate.edu>
2016-07-06 14:26:05 -0700

    Add files via upload

---
+++ algorithm3.cpp
@@ -0,0 +1,94 @@
+#include "algorithm_3.h"
+#include <stdio.h>
+#include <limits.h>
+
+//function finds the maximum of three integers
+struct max_val(struct subarry x,struct subarry y,struct subarry z)
+{
+    //stores the max value of the 3 cases
+    struct subarry A;
+
+
+    //if statements used to find the max value.
+    if(x.sum > y.sum)
+        A.sum = x.sum;
+    else
+        A.sum = y.sum;
+
+    if( max_value < z.sum)
+        A.sum = z.sum;
+
+    return A;
+
+}
+
+//this is to find if the maximum sum crosses over two arrays.
+/*
+A[] - Array being passed to function
+low - start of the array
+mid - middle of array
+high - end of the array
+*/
+struct max_crossing(int A[], int low, int mid, int high)
+{
+    struct subarry A;
+
+
+    int sum = 0; //this variable keeps track of the sums being calculated
+
+    //INT_MIN sets the two below variables to the lowest value or  -2147483648
+    int sum_left_side = INT_MIN;  //this is the sum of the left side
+    int sum_right_side = INT_MIN; //This is the sum of the right side
+
+    //this is a counter for the for loops
+    int i;
+    //This loop will check all the values from the left side.
+    for(i = mid; i >= low; i--)
+    {
+        sum = sum + A[i];
+        if(sum > sum_left_side)
+            sum_left_side = sum;
+    }
+
+    sum = 0; //resets sum back to 0;
+
+    //this loop checks the right hand side
+    for(i = mid+1; i <= high; i++)
+    {
+        sum = sum + A[i];
+        if (sum > sum_right_side)
+            sum_right_side = sum;
+    }
+
+//transfers values to struct
+    A.sum = sum_left_side + sum_right_side;
+    A.highIndex = high;
+    A.lowIndex = low;
+
+//return the sum of the elements
+    return A;
+}
+
+//this function is for finding the max of the array of a non crossing case
+/*
+A[] - Array being passed to function
+low - start of the array
+mid - middle of array
+high - end of the array
+*/
+struct max_array(int A[], int low, int high)
+{
+    //check the base case or 1 element
+    if(low == high)
+        return A[low];
+
+    //this variable is for finding the middle value.
+    int mid = (low + high)/2;
+
+    //this will return the maximum value for the left, right and crossing case
+    return max_val(max_array(A, low, mid),
+                   max_array(A, mid+1, high),
+                   max_crossing(A, low, mid, high)
+                   );
+
+}
---
+++ algorithm3.h
@@ -0,0 +1,38 @@
+#ifndef ALGORITHM_3_H_
+#define ALGORITHM_3_H_
+
+/*
+Struct to hold all the values from the algorithm
+*/
+struct subarry{
+    int lowIndex;
+    int highIndex;
+    int sum;
+};
+
+//function finds the maximum of three integers
+struct max_val(int x, int y, int z);
+
+
+//this is to find if the maximum sum crosses over two arrays.
+/*
+A[] - Array being passed to function
+low - start of the array
+mid - middle of array
+high - end of the array
+*/
+struct max_crossing(int A[], int low, int mid, int high);
+
+//this function is for finding the max of the array of a non crossing case
+/*
+A[] - Array being passed to function
+low - start of the array
+mid - middle of array
+high - end of the array
+*/
+struct max_array(int A[], int low, int high);
+
+
+
+
+#endif // ALGORITHM_3_H_
