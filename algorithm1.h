ba86f60c95a21f6e6558555428c81104685be1e4
bJeffrey <jeffsemail88@gmail.com>
2016-07-06 20:50:44 -0700

    Combined algorithm3.h with algorithm1.h

--- algorithm1.h
+++ algorithm1.h
@@ -15,6 +15,38 @@ int getArraySum(int *array, int startIdx, int endIdx);//gets the sum of an array
 struct subarray alg2Calculate(int *array, const int size);//Calculates the largest subarray in an array
 
 
+/*
+Struct to hold all the values from the algorithm
+*/
+
+//function finds the maximum of three integers
+struct subarray max_val(struct subarray x,struct subarray y,struct subarray z);
+
+
+//this is to find if the maximum sum crosses over two arrays.
+/*
+A[] - Array being passed to function
+low - start of the array
+mid - middle of array
+high - end of the array
+*/
+struct subarray max_crossing(int A[], int low, int mid, int high);
+
+//this function is for finding the max of the array of a non crossing case
+/*
+A[] - Array being passed to function
+low - start of the array
+mid - middle of array
+high - end of the array
+*/
+struct subarray max_array(int A[], int low, int high);
+
+
+
+
+
+
+
 
 
 
