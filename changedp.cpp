2f61722f88443a7d07a7be2390976b6e732af330
flbrasington <brasingf@oregonstate.edu>
2016-07-18 14:46:29 -0700

    Update changedp.cpp

--- changedp.cpp
+++ changedp.cpp
@@ -146,30 +146,3 @@ std::cout << "]\n";
     return m;
 
 };
-
-int coinGreedy(int *c, int *v, int i, int A)
-{
-	int count = 0;
-	while (A > 0)
-	{
-		if (A >= v[i-1])
-		{
-			A -= v[i-1];
-			c[i-1]++;
-			count++;
-		}
-		else if(i > 0)
-		{
-			i--;
-		}
-		else
-		{
-			A -= v[i-1];
-			c[i-1]++;
-			count ++;
-		}
-    }
-
-
-	return count;
-}
